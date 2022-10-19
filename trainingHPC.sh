e61e614fa1bf2d186bd957885148bb3b69be3069
Monchai <tiago.falves98@gmail.com>
2022-10-19 18:17:22 +0100

    Updated training

--- trainingHPC.sh
+++ trainingHPC.sh
@@ -22,7 +22,7 @@ conda install -c conda-forge huggingface_hub
 # run your code (pip install modules on login node; datasets read directly from /net/sharedfolders/datasets)
 # dvc repro
 # python "scripts/txt2img.py "--prompt "Kangaroo dressed in an orange hoodie wearing blue sunglasses in front of the Sidney's Opera House" --plms --ckpt sd-v1-4.ckpt --skip_grid --n_samples 2 --ddim_steps 100
-python scripts/stable_txt2img.py   --ddim_eta 0.0   --n_samples 10   --n_iter 4   --scale 7.0   --ddim_steps 50   --ckpt "./trained_models/2022-10-19T16-57-15_HSIL_203_training_images_10000_max_training_steps_firstNameLastName_token_cell_class_word.ckpt"   --prompt "carcinoma cell"')
+python scripts/stable_txt2img.py   --ddim_eta 0.0   --n_sampSaveles 10   --n_iter 4   --scale 7.0   --ddim_steps 50   --ckpt "./trained_models/2022-10-19T16-57-15_HSIL_203_training_images_10000_max_training_steps_firstNameLastName_token_cell_class_word.ckpt"   --prompt "carcinoma cell"
 # copy results back to your home
 # cp -r /hpc/scratch/$user/my-results ~oepenna person as a masterpiece portrait painting by John Singer Sargent in the style of Rembrandt/my-results
 
